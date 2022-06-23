a0b465afcb1bb6cb38b1ce5bf3696ea18261179d
Michael Schinis <mschinis@gmail.com>
2022-06-23 13:13:03 +0300

    Initial Commit

---
+++ swiftui-uikit/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  swiftui-uikit
+//
+//  Created by Michael Schinis on 23/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-uikit/swiftui_uikitApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_uikitApp.swift
+//  swiftui-uikit
+//
+//  Created by Michael Schinis on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_uikitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
